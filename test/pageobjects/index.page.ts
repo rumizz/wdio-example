d9776dafa56e9cbff317bbf77c66470dbe810dba
rumizz <rumi.zoard@gmail.com>
2024-02-27 00:14:50 +0100

    Add tests for list loading and search input

--- test/pageobjects/index.page.ts
+++ test/pageobjects/index.page.ts
@@ -4,12 +4,33 @@ import Page from './page.js';
 /**
  * sub page containing specific selectors and methods for a specific page
  */
-class LoginPage extends Page {
+class IndexPage extends Page {
     /**
      * define selectors using getter methods
      */
     public get title() {
         return $('#container-orderbrowser---master--masterHeaderTitle-inner');
+    }
+    public get list() {
+        return $('#container-orderbrowser---master--list-listUl');
+    }
+    public async items() {
+        return await $$('#container-orderbrowser---master--list-listUl > li');
+    }
+    public get searchField() {
+        return $('#container-orderbrowser---master--searchField-I');
+    }
+    public get searchSubmit() {
+        return $('#container-orderbrowser---master--searchField-search');
+    }
+    public get listText() {
+        return $('#container-orderbrowser---master--list-nodata-text');
+    }
+    public makeSearch(search: string) {
+        this.searchField.waitForEnabled();
+        this.searchField.click();
+        this.searchField.setValue(search);
+        this.searchSubmit.click();
     }
     /**
      * overwrite specific options to adapt it to page object
@@ -19,4 +40,4 @@ class LoginPage extends Page {
     }
 }
 
-export default new LoginPage();
+export default new IndexPage();
--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -1,10 +1,55 @@
-import { expect } from '@wdio/globals'
-import IndexPage from '../pageobjects/index.page.js'
+import { browser, expect } from '@wdio/globals';
+import IndexPage from '../pageobjects/index.page.js';
 
 describe('Order application', () => {
-    it('should load', async () => {
+
+    beforeEach(async () => {
         await IndexPage.open()
+    })
+    it('load', async () => {
         await expect(IndexPage.title).toBePresent()
+    })
+    it("list orders", async () => {
+        await browser.waitUntil(async () => {
+            const listItems = await IndexPage.items();
+            return listItems.length > 5;
+        }, {
+            timeout: 5000,
+            timeoutMsg: 'Orders did not load within 5 seconds'
+        });
+        const listItems = await IndexPage.items();
+        await expect(listItems).toBeElementsArrayOfSize({ gte: 3 })
+    })
+    describe("Search bar", () => {
+        it("focus and typing", async () => {
+            await IndexPage.searchField.waitForEnabled()
+            await IndexPage.searchField.click()
+            await expect(IndexPage.searchField).toBeFocused()
+            await browser.keys("test")
+            await expect(IndexPage.searchField).toHaveValue("test")
+        })
+        it("run the search by search button", async () => {
+            await IndexPage.searchField.waitForEnabled()
+            await IndexPage.searchField.setValue("asdasdasd")
+            await IndexPage.searchSubmit.click()
+            await expect(IndexPage.listText).toHaveText("No matching order found")
+        })
+        it("run the search by enter", async () => {
+            await IndexPage.searchField.waitForEnabled()
+            await IndexPage.searchField.setValue("asdasdasd")
+            await IndexPage.searchField.click()
+            await browser.keys(['Enter']);
+            await browser.pause(1000)
+            await expect(IndexPage.listText).toHaveText("No matching order found")
+        })
+        it("clear by reset button", async () => {
+            await IndexPage.searchField.waitForEnabled()
+            await IndexPage.searchField.setValue("test")
+            const button = await $("#container-orderbrowser---master--searchField-reset")
+            await button.waitForClickable();
+            await button.click()
+            await expect(IndexPage.searchField).toHaveValue("")
+        })
     })
 })
 
